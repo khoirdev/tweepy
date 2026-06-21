8bb9261a768b1514186e2a4cf6fb411a51eadd9e
khoirdev <khoirulaziz@protonmail.com>
2026-06-21 16:47:04 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ X_ACCESS_SECRET = os.environ["X_ACCESS_SECRET"]
 
 # === GEMINI SETUP ===
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1,5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 # === PROMPT THEMES ===
 themes = [
